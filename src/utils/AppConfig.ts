f23e9bc6422b725324bf3a3770e0a3604d0704f6
war61 <158256278+war61@users.noreply.github.com>
2024-02-01 07:46:27 +0700

    Edit site_name (#2)
    * Delete .github/FUNDING.yml
    
    * change author name
    
    * edit site_name

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,8 +1,8 @@
 export const AppConfig = {
-  site_name: 'Astro boilerplate',
-  title: 'Astro boilerplate',
+  site_name: 'Waranyoo',
+  title: 'Waranyoo',
   description: 'Boilerplate built with Astro using React and Tailwind CSS',
-  author: 'Emma',
+  author: 'war61',
   locale_region: 'en-us',
   locale: 'en',
 };
